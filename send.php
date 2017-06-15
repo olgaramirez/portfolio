0fd83ad97a0f058b6a7619aeb354c145fa886219
olgaramirez <olgaramirez1012@gmail                                                                                                                                                             .com>
2017-06-15 13:31:58 -0400

    'email_deux'

--- send.php
+++ send.php
@@ -1,29 +1,22 @@
+<meta charset="utf">
+<?php $name = $_POST['textinput'];
+$email = $_POST['courriel'];
+$message = $_POST['textarea'];
+$formcontent="ГЂ: $name \n Message : $message";
+$recipient = "olgaramirez1012@gmail.com";
+$subject = $_POST['sujet'];
+$mailheader = "Portfolio : $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Erreur!");
 
-
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$website = $_POST['website'];
-$priority = $_POST['priority'];
-$type = $_POST['type'];
-$message = $_POST['message'];
-$call = $_POST['call'];
-
-$check_msg='';
-foreach($_POST['connect'] as $value) {
-$check_msg .= $value.' ';
-}
-
-$formcontent=" От: $name \n Телефон: $phone \n Обратный звонок: $call \n Сайт: $website \n Способ связи: $check_msg \n Важность: $priority \n Тип: $type \n Сообщение: $message";
-
-$recipient = "anymail@mail.ru";
-$subject = "Форма обратной связи. Пользователь - $name";   
-$mailheader = "Отправитель: $email \r\n";  
-$info = mail($recipient, $subject, $formcontent, $mailheader, "Content-type:text/plain; Content-type:text/plain; charset = UTF-8\r\n") or die("Error!");
-
-echo "Спасибо!" . " -" . "<a href='/demos/myform' style='text-decoration:none;color:#ff0099;'> Назад</a>";
 ?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Lettre envoyГ©e</title>
+</head>
 
+<body>
+<h1><?php echo "Merci!"; ?></h1>
+</body>
+</html>
 
-   
